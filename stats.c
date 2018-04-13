c84e18b243b67ad48ab6b35e2fe3e294e1615018
Sneha <snhnadig@gmail.com>
2018-04-13 14:55:13 -0700

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief <Files contains functions to print contents of an arry, sort the array, find maximum, minimum, mean and median of the array and finally to print the statistics of the array >
  *
- * <Add Extended Description Here>
+ * 
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Sneha Nadig>
+ * @date <4/13/2018 >
  *
  */
 
@@ -23,10 +23,17 @@
 
 #include <stdio.h>
 #include "stats.h"
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* Size of the Data Set */
 #define SIZE (40)
-
+/*Global declaration of variables so they can be used inside in functions as well as main() */
+unsigned char mean;
+unsigned char median;
+unsigned char maximum;
+unsigned char minimum;
 void main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
@@ -35,9 +42,39 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
+
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
-
+  print_array(test);
+  mean=find_mean(test);
+  maximum=find_maximum(test);
+  minimum=find_minimum(test);
+  median=find_median(test);
+  sort_array(test);
+  print_statistics(test);
 }
 
-/* Add other Implementation File Code Here */
+/* This function prints the contents of an array which is passed with pointer p. */
+void print_array( unsigned char *p)
+{}
+/* This function finds the mean of the array which is passed with pointer arr */
+unsigned char find_mean(unsigned char *arr)
+{}
+/* This function finds the maximum value of the array */
+unsigned char find_maximum(unsigned char *arr)
+{}
+/* This function finds the minimum value of the array */
+unsigned char find_minimum(unsigned char *arr)
+{}
+/*This function finds the median of the array. Since the array is unsorted, contents of the array is copied to a temporary pointer, sorted in ascending order and then the median value is calculated */
+void find_median(unsigned char *arr);
+{}
+/*This function sorts the contents of the array which is passed in pointer arr_sort in descending order */
+void sort_array(unsigned char *arr_sort)
+{}
+/* This function prints the statistics of an array which include mean, median, maximum, minimum */
+void print_statistics(unsigned char *arr2)
+{}
+
+
+
--- stats.h
+++ stats.h
@@ -24,17 +24,67 @@
 /* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief <Function to print contents of an array>
  *
- * <Add Extended Description Here>
+ * This function will print the contents of the the array defined in main() in the stst.c file. This function takes an input of type unsigned char *. The pointer variable *arr points to the cintents in the starting address of the array "test" defined in the main function.
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @param <arr> <unsigned char pointer which points to the given array in main()>
+
+ * @return <This function does not have a return type>
  */
+void print_array(unsigned char *arr);
 
+/**
+ * @brief <Function to calcualte the mean of n-elements in an array>
+ *
+ *This function calculates the mean of n-elements in the array. The formula to calculate the mean is as follows:
+ mean=(sum of n-elements in the array)/number of elements in the array.Number of elements in the array is prefined to be 40 with the keyword SIZE in stats.c
+ *
+ * @param <arr> <unsigned char pointer which points to the given array in main()>
 
+ * @return <Returns the value of mean. Return type unsigned char>
+*/
+unsigned char find_mean(unsigned char *arr);
+/**
+* @brief <Function to find the maximum value of an array>
+*
+* This function find the maximum value among n-elemtns in an array). Number of elements in the array is prefined to be 40 with the keyword SIZE in stats.c
+* @param <arr>  <unsigned char pointer which points to the given array in main()>
+* @return <Returns the value of maximum element. Return type unsigned char>
+
+*/
+unsigned char find_maximum(unsigned char *arr);
+/**
+* @brief <Function to find the minimum value of an array>
+*
+* This function finds the minimum value among n-elemtns in an array). Number of elements in the array is prefined to be 40 with the keyword SIZE in stats.c
+* @param <arr>  <unsigned char pointer which points to the given array in main()>
+* @return <Returns the value of minimum element. Return type unsigned char>
+
+*/
+unsigned char find_minimum(unsigned char *arr);
+/**
+* @brief <Function to find the median of an array>
+*
+* This function finds the median value among n-elemtns in an array. Number of elements in the array is prefined to be 40 with the keyword SIZE in stats.c
+* @param <arr>  <unsigned char pointer which points to the given array in main()>
+* @return <Returns the value of median. Return type unsigned char>
+*/
+unsigned char find_median(unsigned char *arr);
+/**
+* @brief <Function to sort an array in descending order>
+*
+* This function sorts the array in descending order. Number of elements in the array is prefined to be 40 with the keyword SIZE in stats.c
+* @param <arr>  <unsigned char pointer which points to the given array in main()>
+* @return <Return type void>
+*/
+void sort_array(unsigned char *arr_sort);
+/**
+* @brief <Function to statistics of an array>
+*
+* This function prints statistics of an array like mean, median, maximum and minimum. Number of elements in the array is prefined to be 40 with the keyword SIZE in stats.c
+* @param <arr>  <unsigned char pointer which points to the given array in main()>
+* @return <Return type is unsigned char pointer>
+*/
+void print_statistics(unsigned char *arr2);
 #endif /* __STATS_H__ */
