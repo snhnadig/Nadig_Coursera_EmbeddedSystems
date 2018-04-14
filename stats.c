a04ef4b4958d139125ca7ce91f38fe0b99d70354
Sneha <snhnadig@gmail.com>
2018-04-13 18:16:35 -0700

    Adding logic for all the functions

--- stats.c
+++ stats.c
@@ -23,7 +23,6 @@
 
 #include <stdio.h>
 #include "stats.h"
-#include <conio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -32,8 +31,8 @@
 /*Global declaration of variables so they can be used inside in functions as well as main() */
 unsigned char mean;
 unsigned char median;
-unsigned char maximum;
-unsigned char minimum;
+unsigned char max;
+unsigned char min;
 void main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
@@ -47,8 +46,8 @@ void main() {
   /* Statistics and Printing Functions Go Here */
   print_array(test);
   mean=find_mean(test);
-  maximum=find_maximum(test);
-  minimum=find_minimum(test);
+  max=find_maximum(test);
+  min=find_minimum(test);
   median=find_median(test);
   sort_array(test);
   print_statistics(test);
@@ -56,25 +55,118 @@ void main() {
 
 /* This function prints the contents of an array which is passed with pointer p. */
 void print_array( unsigned char *p)
-{}
+{
+	int i;
+	printf("Printing contents of the array:\n");
+	for(i=0;i<SIZE;i++)
+	{
+		printf("element[%d]    %d\n",i,*p);
+		*p++;
+	}
+}
 /* This function finds the mean of the array which is passed with pointer arr */
 unsigned char find_mean(unsigned char *arr)
-{}
+{
+	int add=0;
+	int i;
+	unsigned char mean_var=0;
+	for(i=0;i<SIZE;i++)
+	{
+		add=add+(*(arr+i));
+		
+	}
+	mean_var=add/SIZE;
+	return mean_var;
+}
 /* This function finds the maximum value of the array */
 unsigned char find_maximum(unsigned char *arr)
-{}
+{
+	int i;
+	unsigned char max=*arr;
+	for(i=0;i<SIZE;i++)
+	{
+		if(max<*(arr+i))
+		{
+			max=*(arr+i);
+		}
+	}
+	return max;
+}
 /* This function finds the minimum value of the array */
 unsigned char find_minimum(unsigned char *arr)
-{}
+{
+	int i;
+	unsigned char min=*arr;
+	for(i=0;i<SIZE;i++)
+	{
+		if(min>*(arr+i))
+		{
+			min=*(arr+i);
+		}
+	}
+	return min;
+}
 /*This function finds the median of the array. Since the array is unsorted, contents of the array is copied to a temporary pointer, sorted in ascending order and then the median value is calculated */
-void find_median(unsigned char *arr);
-{}
+unsigned char find_median(unsigned char *arr)
+{
+	unsigned char middle, median;
+	unsigned char *ptr= (unsigned char*)malloc(SIZE*sizeof(ptr));
+	int i=0,j;
+	unsigned char temp;
+	while(i<SIZE)
+	{
+		*(ptr+i)=*(arr+i);
+		i++;
+	}
+	for(i=0;i<SIZE;i++)
+	{
+		for(j=(i+1);j<SIZE;j++)
+		{
+			if(*(ptr+i)>*(ptr+j))
+			{
+				temp=*(ptr+i);
+				*(ptr+i)=*(ptr+j);
+				*(ptr+j)=temp;
+			}
+		}
+	}
+	middle= (SIZE+1)/2;
+	median=*(ptr+middle);
+	return median;
+}
+
 /*This function sorts the contents of the array which is passed in pointer arr_sort in descending order */
 void sort_array(unsigned char *arr_sort)
-{}
+{
+	int i,j;
+	unsigned char t=0;
+	for (i=0;i<(SIZE);i++)
+	{
+		for (j=(i+1);j<(SIZE);j++)
+		{
+			if(*(arr_sort+i)<*(arr_sort+j))
+			{
+				t=*(arr_sort+i);
+				*(arr_sort+i)=*(arr_sort+j);
+				*(arr_sort+j)=t;
+			}
+		}
+	}
+	printf("Printing sorted array from largest to smallest\n");
+	for (i=0;i<SIZE;i++)
+	{
+		printf("element[%d]    %d\n",i,*arr_sort);
+		arr_sort++;
+	}
+}
 /* This function prints the statistics of an array which include mean, median, maximum, minimum */
 void print_statistics(unsigned char *arr2)
-{}
+{
+	printf("Mean: %u\n", mean);
+	printf("Median: %d\n", median);
+	printf("Max: %d\n", max);
+	printf("Min: %d\n", min);
+}
 
 
 
